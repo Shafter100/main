3dee527a2fd56c61ee77f9408e15dc0810550a1c
Fedor Shatalov <shatalov@uchi.ru>
2018-12-17 09:45:36 +0300

    Added new lights version

--- leapYear.rb
+++ leapYear.rb
@@ -2,4 +2,4 @@ print 'Enter a year: '
 while (year = gets.chomp.to_i).zero?
   print 'Enter a number: '
 end
-puts (year % 4).zero? ? 'It is a leap year!' : 'It is not a leap year('
+puts ((year % 4).zero? && (year % 100 != 0) || (year % 400).zero?) ? 'It is a leap year!' : 'It is not a leap year('
