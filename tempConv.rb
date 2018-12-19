db507f607d23b3ac0398437e3a95063002ee6f8a
Fedor Shatalov <shatalov@uchi.ru>
2018-12-19 18:00:56 +0300

    Added new version of tempConv.rb

--- tempConv.rb
+++ tempConv.rb
@@ -1,8 +1,3 @@
-print "Convert from Celsius to Kelvin (type \"CK\") or from Kelvin to Celsius (type \"KC\"): "
-until (convMethod = gets.chomp.upcase) == "CK" || convMethod == "KC"
-  print "Type CK or KC: "
-end
-
 def validTemp(convMethod, temp)
   case convMethod
   when "CK"
@@ -19,11 +14,6 @@ def validTemp(convMethod, temp)
   true
 end
 
-print "Input temperature: "
-until ((temp = gets.chomp).to_f.to_s == temp || temp.to_i.to_s == temp) && validTemp(convMethod, temp.to_f)
-  print "Enter a number: "
-end
-
 def conv(convMethod, temp)
   temp = temp.to_f
   case convMethod
@@ -32,7 +22,30 @@ def conv(convMethod, temp)
   when "KC"
     temp -= 273.15
   end
-  puts temp
 end
 
-conv(convMethod, temp)
+def output(convMethod, temp)
+  case convMethod
+  when "CK"
+    puts "#{temp.round(4)} K"
+  when "KC"
+    puts "#{temp.round(4)} ºC"
+  end
+end
+
+def main
+  print "Convert from Celsius to Kelvin (type \"CK\") or from Kelvin to Celsius (type \"KC\"): "
+  until (convMethod = gets.chomp.upcase) == "CK" || convMethod == "KC"
+    print "Type CK or KC: "
+  end
+
+  print "Input temperature: "
+  until ((temp = gets.chomp).to_f.to_s == temp || temp.to_i.to_s == temp) && validTemp(convMethod, temp.to_f)
+    print "Enter a number: "
+  end
+
+  temp = conv(convMethod, temp)
+  output(convMethod, temp)
+end
+
+main
