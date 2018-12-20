267d28fe62585d2ee151c9124d7c61495ebbf1e9
Fedor Shatalov <shatalov@uchi.ru>
2018-12-20 13:54:08 +0300

    New version of tempProbs.rb

--- tProbs.rb
+++ tProbs.rb
@@ -1,12 +1,16 @@
 arrNil = [nil,123,nil,"test", :foo, :bar]
-print arrNil.compact
+#print arrNil.compact
+#puts
+print arrNil - [nil]
 puts
 ######
 arrHash = [:foo, nil, :foo, "foo", 5, 'bar', :foo, 'banana', 'milk', 12, :foo, 'bar', 34, 5, 'banana', 'milk', nil, 12, 45, 'milk']
-hashArr = {}
-hashArr.default = 0
-arrHash.each{|elem| hashArr[elem] += 1}
-print hashArr
+#hashArr = {}
+#hashArr.default = 0
+#arrHash.each{|elem| hashArr[elem] += 1}
+#print hashArr
+#puts
+print arrHash.inject(Hash.new(0)){|resHash, elem| resHash[elem] += 1; resHash}
 puts
 ######
 hashSort = { abc: 'hello', 'another_key' => 123, 4567 => 'third' }
