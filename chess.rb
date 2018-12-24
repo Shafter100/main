80082fa1ef33f2a3666ef80f9cda342d6eec6d7c
Fedor Shatalov <shatalov@uchi.ru>
2018-12-24 16:58:04 +0300

    New version of chess.rb

--- chess.rb
+++ chess.rb
@@ -1,36 +1,61 @@
+LCornerUp = "\u250c"
+HLine = "\u2500"
+RCornerUp = "\u2510"
+VLine = "\u2502"
+WhiteBox = "\u2591"
+BlackBox = "\u2588"
+LCornerDown = "\u2514"
+RCornerDown = "\u2518"
+
 def chessBoard
   def letters
-    4.times{print " "}
-    ("A".."H").each{|ch| print ch + " "}
+    (N.to_s.size + 4).times{print " "}
+    #("A"..("A".ord + N - 1).chr).each{|ch| print ch + " "}
+    (0..(N - 1)).each do |ch|
+      if ch < 26
+        print (ch + 65).chr + "  "
+      else
+        print (ch%26 + 65).chr + (ch / 26).to_s + " "
+      end
+     end
     puts
   end
 
   letters
 
-  2.times{print " "}
-  print "\u250c"
-  17.times{print "\u2500"}
-  print "\u2510"
-  puts
-  
-  for i in (0..7)
-    print "#{8 - i} \u2502 "
-    if (8 - i).even?
-      4.times{print "\u2591 \u2588 "}
-    else
-      4.times{print "\u2588 \u2591 "}
-    end
-    print "\u2502 #{8 - i}"
+  def heading(lCorner, rCorner)
+    (N.to_s.size + 1).times{print " "}
+    print lCorner
+    (3 * N + 2).times{print HLine}
+    print rCorner
     puts
   end
 
-  2.times{print " "}
-  print "\u2514"
-  17.times{print "\u2500"}
-  print "\u2518"
-  puts
+  def fillField(even)
+    def comPrint(c1, c2)
+      #(N / 2).times{print "#{c1} #{c2} "}
+      (0...(N / 2)).to_a.each do
+        print "#{c1}  #{c2}  "
+      end
+      print "#{c1}  " if N.odd?
+    end
+    even ? comPrint(WhiteBox, BlackBox) : comPrint(BlackBox, WhiteBox)
+  end
+
+  heading(LCornerUp, RCornerUp)
+
+  (0..(N - 1)).to_a.each do |i|
+    (N.to_s.size - (N - i).to_s.size).times {print " "}
+    print "#{N - i} #{VLine}  "
+    fillField((N - i).even?)
+    print "#{VLine} #{N - i}"
+    puts
+  end
+
+  heading(LCornerDown, RCornerDown)
 
   letters
 end
 
+N = 8
 chessBoard
