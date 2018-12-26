6007a938ffd044de0dc4656108fb0aff490f1b68
Fedor Shatalov <shatalov@uchi.ru>
2018-12-26 15:47:58 +0300

    New version of charCount.rb

--- charCount.rb
+++ charCount.rb
@@ -1,26 +1,17 @@
-#Решение 1
-def count1(f)
-  # Вывод в виде хеша
-  #puts f.chars.to_a.inject(Hash.new(0)){|resHash, elem| resHash[elem] += 1; resHash}
-  # Вывод в стоблец
-  f.chars.to_a.inject(Hash.new(0)){|resHash, elem| resHash[elem] += 1; resHash}.each {|k, v| puts "#{k}: #{v}"}
-end
-#Решение 2
-def count2(f)
-  freqs = Hash.new(0)
+def count(f, freqs)
   f.each_char {|ch| freqs[ch] += 1}
-  #puts freqs # Вывод в виде хеша
-  # Вывод в стоблец
-  freqs.each {|k, v| puts "#{k}: #{v}"}
 end
 
-def main
-  f = File.open("text.txt", "r") if File.file?("text.txt")
-  f = f.read
-  puts "Method 1: "
-  count1(f)
-  puts "\nMethod 2: "
-  count2(f)
+def countSym(filePath)
+  #f = File.open(filePath, "r") if File.file?(filePath)
+  #f = f.read
+  if File.file?(filePath)
+    freqs = Hash.new(0)
+    File.foreach(filePath) { |line| count(line, freqs)}
+    freqs.each {|k, v| puts "#{k}: #{v}"}
+  else
+    puts "File does not exist!"
+  end
 end
 
-main
+countSym("text.txt")
