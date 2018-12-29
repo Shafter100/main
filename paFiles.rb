0078ac2891c439df621853b7e69e51cf2f0aa2be
Fedor Shatalov <shatalov@uchi.ru>
2018-12-29 10:48:20 +0300

    New version of paFiles.rb

--- paFiles.rb
+++ paFiles.rb
@@ -5,8 +5,6 @@ def oneRound(user1Choice, user2Choice, items)
 end
 
 def userWon(user1Choice, user2Choice, items)
-  #user2Choice = items[rand(1..3).to_s]
-  #puts "I put #{user2Choice}!"
   case user1Choice
   when "paper"
     case user2Choice
@@ -39,6 +37,18 @@ def userWon(user1Choice, user2Choice, items)
 
 end
 
+def generateFiles(items)
+  user1File = File.new("user1.txt", "w+")
+  user2File = File.new("user2.txt", "w+")
+  games = 10
+  games.times do
+    3.times {user1File.puts(items[rand(1..3).to_s])}
+    3.times {user2File.puts(items[rand(1..3).to_s])}
+  end
+  user1File.close
+  user2File.close
+end
+
 def main
   puts "\nLet's play paper, scissors, stone!"
   puts "1. Yep, let's do it!\n2. Nope.."
@@ -56,26 +66,15 @@ def main
 
   items = {"1" => "paper", "2" => "scissors", "3" => "stone"}
 
-  user1File = File.new("user1.txt", "w+")
-  user2File = File.new("user2.txt", "w+")
-  games = 10
-  games.times do
-    3.times {user1File.puts(items[rand(1..3).to_s])}
-    3.times {user2File.puts(items[rand(1..3).to_s])}
-  end
-  user1File.close
-  user2File.close
+  generateFiles(items)
 
   roundNum = 0
   user1Points = 0
-  # цикл по файлам
-
 
   file1Info = File.read("user1.txt").split("\n")
   file2Info = File.read("user2.txt").split("\n")
 
   (0...file1Info.size).each do |i|
-    #while roundNum < 3
       user1Points += oneRound(file1Info[i], file2Info[i], items)
       roundNum += 1
       if roundNum % 3 == 0
@@ -90,13 +89,7 @@ def main
         roundNum = 0
         user1Points = 0
       end
-    #end
-
-    # puts "\nDo you want to play again?"
-    # puts "1. Yes\n2. No"
-    # exitGame
   end
-  # конец цикла по файлам
 end
 
 main
